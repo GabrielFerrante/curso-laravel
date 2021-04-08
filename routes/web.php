a333ad15859bedaa0f7478b44e7ea2044121c01b
Gabriel Ferrante <gabrielsferrante@hotmail.com>
2021-04-08 00:48:17 -0300

    entendendo rotas 07/04/21

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Rota Match permite requisições de acordo com o verbo HTTP específicado
+Route::match(['post', 'get'],'/match', function(){ 
+    return 'Match'; 
+});
+//Rota ANY permite requisições de qualquer verbo HTTP
+//TER CAUTELA NO USO, PODE 
+Route::any('/any', function(){ 
+    return 'Any'; 
+});
+
+Route::get('/empresa', function (){
+    return 'Sua empresa';
+});
+
+Route::get('/contato', function (){
+    return view('site.contact');
+});
 
 Route::get('/', function () {
+    //função de callback como parâmetro para a rota GET
     return view('welcome');
 });
